7bf4bb46f4bf797b6812cfb9c9965e8a09d11e5b
Owen Samuel <owensamuel31@gmail.com>
2024-12-06 21:19:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cobaroom/database/daftarBelanja.kt
@@ -0,0 +1,25 @@
+package com.example.cobaroom.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class daftarBelanja(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0,
+
+    @ColumnInfo(name="tanggal")
+    var tanggal: String? = null,
+
+    @ColumnInfo(name="item")
+    var item: String? = null,
+
+    @ColumnInfo(name="jumlah")
+    var jumlah: String? = null,
+
+    @ColumnInfo(name="status")
+    var status: Int = 0,
+
+)
